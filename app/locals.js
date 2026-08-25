37067b94a20acbd365bd0f123a7ccfd6eefb0b6f
Frankie Roberto <frankie.roberto1@nhs.net>
2026-08-25 14:49:35 +0100

    Add nhsapp-frontend version number (#3)
    This is helpful for debugging and seeing which versions of the key
    dependencies are currently being used

--- app/locals.js
+++ app/locals.js
@@ -1,4 +1,9 @@
+const nhsappFrontendPkg = require('nhsapp-frontend/package.json')
+
 module.exports = function(req, res, next) {
+
+  // Add the version of the NHS app frontend being used
+  res.locals.nhsappFrontendVersion = `v${nhsappFrontendPkg.version}`
 
   // You can set any additional local variables here.
   // These will be made available to any views
