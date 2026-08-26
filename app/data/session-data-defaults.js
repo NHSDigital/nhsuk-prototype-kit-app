7ed3b000c61874fa2af53310f42fb40f947c57ad
Frankie Roberto <frankie.roberto1@nhs.net>
2026-08-26 16:34:21 +0100

    Use sentTime instead of date
    This matches the live API, and allows us to add the time in future

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -4,7 +4,7 @@ module.exports = {
   messages: [
     {
       id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      date: "2025-10-21",
+      sentTime: "2025-10-21",
       sender: "Portland Street Great Westood Surgery",
       content: "Reminder of your telephone appointment with your GP on Friday 23 October at 1:30pm.",
       read: false,
