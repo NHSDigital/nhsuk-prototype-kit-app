11718cd04acd2cad29383beb31b6be3644605193
Frankie Roberto <frankie.roberto1@nhs.net>
2026-08-26 16:45:43 +0100

    Add minimal messages section (#6)
    This uses session-data and is enough for a quick prototype

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,4 +1,15 @@
 // Use this file to set any default data
 
 module.exports = {
+  messages: [
+    {
+      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      sentTime: "2025-10-21",
+      sender: "Portland Street Great Westood Surgery",
+      content: "Reminder of your telephone appointment with your GP on Friday 23 October at 1:30pm.",
+      read: false,
+      flagged: false,
+      removed: false
+    }
+  ]
 }
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,19 @@ const express = require('express')
 
 const router = express.Router()
 
+
+// Show an individual message and mark it as read
+router.get('/messages/:id', (req, res) => {
+  const messageId = req.params.id
+  const data = req.session.data
+  const messages = data.messages || []
+  const message = messages.find(message => message.id === messageId)
+
+  message.read = true
+
+  res.render('message', { message })
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
