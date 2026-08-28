efc28e37c30f3a4e1a65ca7d525e8bf2b7aaea07
Frankie Roberto <frankie.roberto1@nhs.net>
2026-08-28 11:24:10 +0100

    Add native bottom navigation mock (#10)
    This adds a mockup of the native bottom-navigation component, as an
    alternative to the web version

--- app/locals.js
+++ app/locals.js
@@ -5,6 +5,9 @@ module.exports = function(req, res, next) {
   // Add the version of the NHS app frontend being used
   res.locals.nhsappFrontendVersion = `v${nhsappFrontendPkg.version}`
 
+  // Used to select the icon state in the bottom navigation
+  res.locals.unreadMessages = () => Boolean(req.session.data.messages?.some((message) => message.read === false))
+
   // You can set any additional local variables here.
   // These will be made available to any views
   //
